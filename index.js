62b5653ae5e17f1c7daf7cc0ab1d446619e41779
Joaopster <jpster@pop-os.localdomain>
2022-09-15 23:06:54 -0300

    Primeiro teste do MUSTACHE no README 2

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const Mustache = require('mustache');
+const fetch = require('node-fetch');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
 
 let DATA = {
   name: 'João Pster',
-  date: new Date().toLocaleDateString('pt-BR', {
+  refresh_date: new Date().toLocaleDateString('pt-BR', {
     weekday: 'long',
     month: 'long',
     day: 'numeric',
@@ -15,12 +17,56 @@ let DATA = {
   }),
 };
 
-function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+async function setWeatherInformation() {
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+  )
+    .then(r => r.json())
+    .then(r => {
+      DATA.city_temperature = Math.round(r.main.temp);
+      DATA.city_weather = r.weather[0].description;
+      DATA.city_weather_icon = r.weather[0].icon;
+      DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'America/Sao_Paulo',
+      });
+      DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'America/Sao_Paulo',
+      });
+    });
+}
+
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
     fs.writeFileSync('README.md', output);
   });
 }
 
-generateReadMe();
+async function action() {
+  /**
+   * Fetch Weather
+   */
+  await setWeatherInformation();
+
+  /**
+   * Get pictures
+   */
+  // await setInstagramPosts();
+
+  /**
+   * Generate README
+   */
+  await generateReadMe();
+
+  /**
+   * Fermeture de la boutique 👋
+   */
+  // await puppeteerService.close();
+}
+
+action();
